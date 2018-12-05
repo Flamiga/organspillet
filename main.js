2e41d6fa570157aab64b4c8517f96cc108a973ce
Unknown <eaafla@students.eaaa.dk>
2018-12-05 09:09:02 +0100

    seperer javascript

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+$(document).ready(function() { // indlæser siden før javascriptet afspilles
 
+  $('.boble').hide(); // gemmer boblen
+
+  // wait five 0.2 sekunder then show
+  setTimeout( // funktionsnavn
+    function() {
+      //do something special
+      $('.boble').fadeToggle("fast", 200) // indsætter fast ind
+    }, 2000);
+
+  $("#knap").click(function() { // henter id og hvilke handling der ønskes af denne id
+    $(".boble").hide();         // id hentets fra det ønsket indhold som skal skjules
+  });
+});                             //afslutningstag af indlæsning af siden
